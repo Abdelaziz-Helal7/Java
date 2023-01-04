e376b12f3a1b6e644b86c5e8697ba122edfd62b7
Abdelaziz-Helal7 <119802754+Abdelaziz-Helal7@users.noreply.github.com>
2023-01-04 13:47:47 +0200

    Add files via upload
    Barking Dog

--- Main.java
+++ Main.java
@@ -1,36 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isLeapYear(-1600));
-        System.out.println(isLeapYear(1600));
-        System.out.println(isLeapYear(2017));
-        System.out.println(isLeapYear(2000));
+        System.out.println(shouldWakeUp(true,1));
+        System.out.println(shouldWakeUp(false,2));
+        System.out.println(shouldWakeUp(true,8));
+        System.out.println(shouldWakeUp(true,-1));
     }
-    public static boolean isLeapYear (int year)
+    public static boolean shouldWakeUp(boolean barking , int hourOfDay)
     {
-        if (year < 1 || year > 9999) {
-            return false;
+        if (hourOfDay < 0 || hourOfDay > 23)
+        {
+            return false ; 
+        } else if (hourOfDay < 8 || hourOfDay > 22) {
+            return true ;
         }
         else {
-            if (year % 4 == 0)
-            {
-                if (year % 100 == 0 )
-                {
-                    if (year % 400 == 0)
-                    {
-                        return true ;
-                    }
-                    else {
-                        return  false ;
-                    }
-                }
-                else {
-                    return true ;
-                }
-            }
-            else {
-                return false ;
-            }
+            return false ;
         }
-
     }
 }
